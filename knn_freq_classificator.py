ee75f11b90c73f15ca5e0522cbf4bc735a330d28
Csongi <56769659+ShongiX@users.noreply.github.com>
2023-06-14 22:40:18 +0200

    Added KNN classificators

---
+++ knn_freq_classificator.py
@@ -0,0 +1,56 @@
+import os
+import librosa
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+# Define the path to the audio files
+data_path = 'audio/noised_batch/'
+
+# Define the classes
+classes = ['CommonBlackbird', 'WhiteStork']
+
+# Initialize empty lists to store features and labels
+features = []
+labels = []
+
+# Extract features from audio files
+for filename in os.listdir(data_path):
+    if filename.endswith('.wav'):
+        file_path = os.path.join(data_path, filename)
+        waveform, sample_rate = librosa.load(file_path, sr=None)
+        # Extract desired features from waveform
+        feature_vector = librosa.feature.mfcc(y=waveform, sr=sample_rate)
+
+        features.append(feature_vector.flatten())
+        # Extract the class label from the filename
+        label = filename.split('_')[0]
+        labels.append(label)
+
+# Convert feature and label lists to numpy arrays
+max_length = max(len(lst) for lst in features)
+features_padded = []
+for lst in features:
+    lst_array = np.array(lst).reshape(1, -1)  # Convert to (1, length)
+    padding = np.zeros((1, max_length - lst_array.shape[1]))  # Pad with zeros
+    padded_lst = np.concatenate((lst_array, padding), axis=1)  # Concatenate
+    features_padded.append(padded_lst)
+
+X = np.squeeze(np.array(features_padded))
+y = np.array(labels)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the KNN classifier for different k values
+for k in range(1, 6):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+
+    # Make predictions on the test set
+    y_pred = knn.predict(X_test)
+
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy for k =", k, ":", accuracy)
